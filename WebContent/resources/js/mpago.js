62f35e7dc7b7f79717b37ce68f1059542b93e0a9
Unknown <sylux_1808@hotmail.com>
2018-07-11 15:28:36 -0500

    me correran

--- WebContent/resources/js/mpago.js
+++
@@ -1,8 +0,0 @@
-function changeTab(evt, tab) {
-	var activeTabs = $('.active');
-	activeTabs.each(function() {
-		$(this).removeClass('active');
-	})
-	var toActivete = $('#' + tab);
-	toActivete.addClass('active');
-}
--- src/mx/redpoint/isa/security/config/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package mx.redpoint.isa.security.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
